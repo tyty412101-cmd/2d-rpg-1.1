cad76632336e884bad1ca34dd72d0f7511004c1c
tyty412101-cmd <tyty412101@gmail.com>
2026-04-20 05:57:28 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+export const CONFIG = {
+    worldWidth: 4000,
+    worldHeight: 4000,
+
+    player: {
+        speed: 6,
+        maxHealth: 100,
+        damage: 25,
+        range: 60,
+        cooldown: 0.4
+    },
+
+    enemy: {
+        count: 25,
+        speed: 1.5,
+        damage: 10,
+        health: 50,
+        detection: 250
+    }
+};
